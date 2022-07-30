4438b6b87754ec4799b613b0e9d05ff9b612fef9
Vittorio <vittfassano@gmail.com>
2022-07-30 14:53:40 -0300

    Version1

---
+++ src/componentes/Horario.js
@@ -0,0 +1,56 @@
+import { Link, useParams } from "react-router-dom"
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+
+export default function Horario() {
+
+    const { idFilme } = useParams()
+    const [filme, setFilme] = useState([])
+    const [footer, setFooter] = useState([])
+
+    useEffect(() => {
+
+        const promise = axios.get(`https://mock-api.driven.com.br/api/v5/cineflex/movies/${idFilme}/showtimes`);
+        promise.then(response => {
+            console.log("vim da API", response.data.days)
+            setFilme(response.data.days)
+            setFooter(response.data)
+
+        })
+    }, [])
+
+    return (
+        <>
+            <h2>Selecione o horário</h2>
+
+            {filme.map((dia) => {
+                const { weekday, date, showtimes } = dia;
+
+                return <session className="tela-sessoes">
+                    <div className="dia-data">
+                        <span>{weekday} - {date} </span>
+                    </div>
+
+                    <div className="boxes">
+                        {showtimes.map((hora) => {
+                            const { name, id } = hora
+                            
+                            return  <Link to={`/assentos/${id}`} style={{ textDecoration: 'none' }}>
+                            <div className="box"><p className="horarios">{name}</p></div>
+                            </Link>
+                        })}
+                    </div>
+
+                    <div className="fundo">
+                        <div className="moldura-img-fundo">
+                            <img className="img-fundo" src={footer.posterURL}></img>
+                        </div>
+                        <p>{footer.title}</p>
+                    </div>
+
+                </session>
+            })}
+        </>
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,11 @@
+/*import axios from 'axios';
+import { BrowserRouter, Routes, Route } from "react-router-dom";*/
+
+import ReactDOM from 'react-dom';
+
+import App from './componentes/App';
+
+import './assets/css/reset.css';
+import './assets/css/style.css';
+
+ReactDOM.render(<App />, document.querySelector('.root'));
